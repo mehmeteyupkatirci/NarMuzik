f864f0f4c55ab0eb3bd96be7c04658f228f02aed
Mehmet Eyüp Katırcı <eyupbey48>
2020-04-20 22:41:35 +0300

    views - album(home-detail)

---
+++ resources/views/album/detail.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.master')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-sm-6">
+            <div class="msl-blog-full blog-small">
+                <figure>
+                    <img src="\extra-images\masonry-blog5.jpg" alt="KODEFOREST">
+                    <span class="editor-label">ARTIST</span>
+                </figure>
+                <div class="text">
+                    <h5 class="blog-title"><a href="{{route ('artist_detail',$albums->artist_id)}}">{{$albums->artist->name}}</a></h5>
+                    <p> Genres : {{$albums->artist->genres}} <br/>
+                        Popularity : 100 % {{$albums->artist->popularity}}<br/>
+                        Relaese Date : {{$albums->created_at}}<br/>
+                        Relaese Date : {{$albums->updated_at}}<br/>
+                    </p>
+                    <a class="btn-1" href="{{route ('artist_detail',$albums->artist_id)}}">Show More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6"></div>
+        <div class="col-md-4 col-sm-6">
+            <div class="msl-blog-full blog-small">
+                <figure>
+                    <img src="\extra-images\product-img02.jpg" alt="KODEFOREST">
+                    <span class="editor-label">ALBUM</span>
+                </figure>
+                <div class="text">
+                    <h5 class="blog-title"><a href="{{route ('artist_detail',$albums->artist_id)}}">{{$albums->name}}</a></h5>
+                    <p> Genres : {{$albums->genres}} <br/>
+                        Copytrights : {{$albums->copytrights}}<br/>
+                        Popularity : 100 % {{$albums->popularity}}<br/>
+                        Relaese Date : {{$albums->release_date}}<br/>
+                    </p>
+                </div>
+            </div>
+        </div>
+@endsection
---
+++ resources/views/album/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.master')
+@section('content')
+<div class="kode_content_wrap">
+    <section class="artist-section">
+        <div class="container">
+           <div class="row">
+                @if ($albums)
+                    @foreach ($albums as $album)
+                    <div class="col-md-3">
+                        <div class="latest-album">
+                            <article>
+                                <figure><a href="#"><img src="extra-images\albumkapak.jpg" alt=""></a></figure>
+                                <div class="text webkit">
+                                    <div class="left-sec">
+                                    <h6><a href="{{route ('detail',$album->id)}}">{{$album->name}}</a></h6>
+                                        <p>{{$album->genres}}</p>
+                                    </div>
+                                    <a class="icon-plus webkit" href="{{route ('artist_detail',$album->artist_id)}}"><i class="fa fa-plus fa-2x"></i></a>
+                                    <a class="icon-plus webkit" style="margin-right: 5px;" href="{{route ('detail',$album->id)}}"><i class="fa fa-search fa-2x"></i></a>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                    @endforeach
+                @endif
+                 <!--Load More Start-->
+                <div class="col-md-12">
+                   
+                </div>
+                <!--Load More End-->
+           </div>
+        </div>
+    </section>
+</div>
+<!--Main Content Wrap End-->
+@endsection
